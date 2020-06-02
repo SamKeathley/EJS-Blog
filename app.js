af962801f64a7b800580aa5d507373b91badda80
Sam Keahley <samncollette@gmail.com>
2020-06-01 22:15:11 -0700

    completed routing logic and assigment

--- app.js
+++ app.js
@@ -3,10 +3,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Gunwalls Barbary Coast hogshead keel topmast gun tackle grog blossom skysail transom. Clap of thunder lateen sail quarterdeck cackle fruit ahoy mizzen brigantine yawl run a rig rutters. Sink me bring a spring upon her cable hardtack sloop fluke topsail chase yard wench deadlights. Black jack chantey long clothes draft scuppers jack matey lanyard crimp bounty. Aft gangplank provost gally cackle fruit chantey gangway Corsair take a caulk walk the plank.";
+const aboutContent = "Spike pink yardarm sutler me crow's nest lass square-rigged hulk bilge. Brigantine scurvy broadside jolly boat nipperkin clipper hornswaggle hulk main sheet holystone. Reef case shot American Main splice the main brace bowsprit snow trysail gun man-of-war spike. Shiver me timbers tackle hulk fluke port ballast no prey, no pay walk the plank plunder pinnace. Mutiny take a caulk clap of thunder Chain Shot smartly sutler reef yard hornswaggle spyglass. ";
+const contactContent = "Hail-shot mutiny lookout spyglass crow's nest long boat gaff spirits piracy Spanish Main. Nelsons folly bilge haul wind Jolly Roger cog quarter gibbet fathom Arr Jack Tar. Carouser loaded to the gunwalls driver rigging brigantine grog marooned haul wind hornswaggle rope's end. Careen yawl grapple ho Corsair doubloon coxswain Jack Ketch spirits cutlass. Plate Fleet poop deck Buccaneer American Main Jack Ketch tackle squiffy Privateer yard code of conduct.";
 
 const app = express();
 
@@ -45,7 +46,21 @@ app.post("/compose", function(req, res) {
   res.redirect("/");
 });
 
+app.get("/posts/:postName", function(req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
 
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    };
+  });
+
+});
 
 app.listen(3000, function() {
   console.log("Server started on http://localhost:3000");
